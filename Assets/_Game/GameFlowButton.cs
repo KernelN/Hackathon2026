0ef93938245fcca82939c009c0517a58bdf474f8
Alejandro Insausti <alejandroninsausti@gmail.com>
2026-07-02 20:35:16 -0300

    Add Ink

---
+++ Assets/_Game/GameFlowButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Hackathon
+{
+    public class GameFlowButton : MonoBehaviour
+    {
+        [SerializeField] Scene sceneToChange;
+        public void ChangeScene()
+        {
+            if(Time.timeScale == 0) Time.timeScale = 1;
+            SceneManager.LoadScene(sceneToChange.name);
+        }
+
+        public void Quit() => Application.Quit();
+
+        public void Pause() => Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+}
---
+++ Assets/_Game/TestInk.ink
@@ -0,0 +1,8 @@
+-> knotName
+=== knotName ===
+* [A choice where the content isn't printed after choosing]
+-> stitchName
+
+= stitchName
+* [This is the content of the stitch that should be embedded within a knot.]
+-> END
